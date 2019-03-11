0b5796c67cef52ab89ec0af4a2f241bc9e3dc1f3
wharsojo <wharsojo@gmail.com>
2019-03-11 20:09:04 +0800

    update scripts

--- termux-setup.sh
+++ termux-setup.sh
@@ -4,7 +4,7 @@ linuxFsSetup=./ubuntu-fs$linuxSetup
 github="https://raw.githubusercontent.com"
 myLinux="$github/Neo-Oli/termux-ubuntu/master/ubuntu.sh"
 ohMyZsh="$github/robbyrussell/oh-my-zsh/master/tools/install.sh"
-pkg i vi sed curl git zsh tmux proot openssh -y
+pkg i vi git sed zsh curl wget tmux proot openssh -y
 
 echo "\nInstall oh-my-zsh"
 sh -c "$(curl -fsSL $ohMyZsh)"
@@ -13,13 +13,13 @@ chsh -s zsh
 echo "\nInstall Ubuntu Linux"
 mkdir -p $linuxPath && cd $linuxPath && sh -c "$(curl -fsSL $myLinux)"
 echo "[ -s \"$linuxSetup\" ] && sh $linuxSetup" >> $linuxPath/ubuntu-fs/root/.bashrc
+
 echo "alias ubuntu='(cd $linuxPath && ./start-ubuntu.sh)'" >> ~/.zshrc
 echo "tmux new-session -d -s scr" >> ~/.zshrc
 echo "sshd" >> ~/.zshrc
 
-echo "apt-get update -y" > $linuxFsSetup
-echo "apt-get upgrade -y" >> $linuxFsSetup
-echo "apt-get install vi curl git zsh -y" >> $linuxFsSetup
+echo "apt-get update -y && apt-get upgrade -y" > $linuxFsSetup
+echo "apt-get install vi git zsh curl -y" >> $linuxFsSetup
 echo "$(curl -fsSL $ohMyZsh)" >> $linuxFsSetup
 echo "rm ~/.ubuntu-setup.sh" >> $linuxFsSetup
 sed -i 's/chsh -s/# chsh -s/g' $linuxFsSetup
