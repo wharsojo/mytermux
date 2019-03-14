82ae2cf92950f8a19d0ed2c197e0208969b1e7d5
wharsojo <wharsojo@gmail.com>
2019-03-15 03:57:14 +0800

    add scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@ mytermux="/data/data/com.termux/files/home/.mytermux"
 scripts="$mytermux/scripts"
 
 env git clone --depth=1 https://github.com/wharsojo/mytermux $mytermux
-sh -c $(cat $scripts/setup-termux.sh)
+sh -c $(cat $scripts/termux-setup.sh)
