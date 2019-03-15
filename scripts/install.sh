4aa7b3078f085c691fb0d404bf0547c771a204dd
wharsojo <wharsojo@gmail.com>
2019-03-15 08:19:34 +0800

    add scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,9 @@
-mytermux="/data/data/com.termux/files/home/.mytermux"
+#!/data/data/com.termux/files/usr/bin/bash -e
+
+mytermux="/data/data/com.termux/files/home/mytermux"
 scripts="$mytermux/scripts"
 
-env git clone --depth=1 https://github.com/wharsojo/mytermux $mytermux
-`$scripts/termux-setup.sh` 
+## env git clone --depth=1 https://github.com/wharsojo/mytermux $mytermux
+echo $scripts/termux-setup.sh 
+`$scripts/termux-setup.sh`
+
--- scripts/termux-setup.sh
+++ scripts/termux-setup.sh
@@ -1,12 +1,24 @@
 #!/data/data/com.termux/files/usr/bin/bash -e
 
-## Setup colors
-reset='\033[0m'
-red='\033[1;31m'
-blue='\033[1;34m'
-yellow='\033[1;33m'
+## Setup Colors printf "\x1b[38;2;255;100;0mTRUECOLOR\x1b[0m\n"
+reset='\x1b[0m'
+red='\x1b[1;31m'
+blue='\x1b[1;34m'
+yellow='\x1b[1;33m'
 
-printf "$yellow"
-printf "[*] Install termux packages ..."
-pkg i vim git sed zsh curl wget tmux proot openssh -y
+printf "$yellow [*] Install termux packages ...$reset\n"
+# pkg i vim git sed zsh curl wget tmux proot openssh -y
+
+printf "$yellow [*] Setup storage ...$reset\n"
+#termux-setup-storage
+
+## Install oh-my-zsh on termux
+printf "$yellow [*] Setup oh my zshell ...$reset\n"
+github="https://raw.githubusercontent.com"
+ohMyZsh="$github/robbyrussell/oh-my-zsh/master/tools/install.sh"
+# sh -c "$(curl -fsSL $ohMyZsh)"
+# chsh -s zsh
+
+printf "$yellow [*] Setup default startup$reset\n"
+cp zshenv ~/.zshenv
 
