fe16e200d2453483c42588b4d6bf84758c8eb397
wharsojo <wharsojo@gmail.com>
2019-03-16 04:58:22 +0700

    add scripts

---
+++ scripts/zzz/test.sh
@@ -0,0 +1,19 @@
+#!bash -e
+#echo $(uname -m)
+#if [ $(uname -m)="aarch64" ]; then
+#  typ="arm linux"
+#fi
+#
+#echo $typ
+#echo "SDCARD: [$(ls /storage | grep -E '[0-9]+')]"
+#
+#cd ~/linux/ubuntu
+#curl $MYTERMUX/start-ubuntu.sh > start-ubuntu2.sh
+#chmod +x start-ubuntu.sh
+
+lolipop() {
+  echo lolipoppp
+}
+
+lolipop
+
