7c09d390097b338c011e096a72683778c8a2ef8e
wharsojo <wharsojo@gmail.com>
2019-03-17 18:50:43 +0800

    update scripts

--- scripts/gitea.sh
+++ scripts/gitea.sh
@@ -1,8 +1,17 @@
-## Download gitea app
+#!/data/data/com.termux/files/usr/bin/bash -e
+
+## Setup Colors
+reset='\033[0m'
+red='\033[1;31m'
+blue='\033[1;34m'
+yellow='\033[1;33m'
+
 URL="https://dl.gitea.io/gitea/1.7.4"
 file="gitea-1.7.4-linux-arm64"
 FS="$HOME/.mytermux/gitea-app"
 
+## Download gitea app
+printf "$yellow [*] Download: $file ...$reset\n"
 rm -rf $FS
 mkdir -p $FS && cd $FS
 curl --progress-bar -L --fail --retry 4 -O "$URL/$file" 
