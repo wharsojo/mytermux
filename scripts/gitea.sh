4130265a94ab105dc004a6a266afe92fee41e531
wharsojo <wharsojo@gmail.com>
2019-03-17 18:43:42 +0800

    update scripts

--- scripts/gitea.sh
+++ scripts/gitea.sh
@@ -1,11 +1,11 @@
 ## Download gitea app
-URL="https://dl.gitea.io/gitea/1.7.4/gitea-1.7.4-linux-arm64"
+URL="https://dl.gitea.io/gitea/1.7.4"
+file="gitea-1.7.4-linux-arm64"
 FS="$HOME/.mytermux/gitea-app"
-file="gitea"
 
 rm -rf $FS
 mkdir -p $FS && cd $FS
-curl --progress-bar -L --fail --retry 4 -O "$URL/$file" -o $file
-chmod +x $file
+curl --progress-bar -L --fail --retry 4 -O "$URL/$file" -o gitea
+chmod +x 'gitea'
 cd ..
 
