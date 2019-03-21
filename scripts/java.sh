1c65fbe54331d9f594fc37a8224a8e10b2227c09
wharsojo <wharsojo@gmail.com>
2019-03-22 07:18:01 +0800

    update  scripts

---
+++ scripts/java.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/bash -e
+
+## Setup Colors
+reset='\033[0m'
+red='\033[1;31m'
+blue='\033[1;34m'
+yellow='\033[1;33m'
+
+URL="https://download.java.net/java/GA/jdk11/13/GPL"
+file="openjdk-11.0.1_linux-x64_bin.tar.gz"
+FS="$HOME/.mytermux/java-vm"
+
+## Download java-vm
+printf "$yellow [*] Download: $file ...$reset\n"
+rm -rf $FS
+mkdir -p $FS && cd $FS
+curl --progress-bar -L --fail --retry 4 -O "$URL/$file" 
+tar -zxf $file
