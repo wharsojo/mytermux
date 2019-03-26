d82c776642c1c3a4d42a93e35b1c95a8e29ddb5f
wharsojo <wharsojo@gmail.com>
2019-03-27 05:42:51 +0800

    add fedora & update scripts

---
+++ linux/fedora/setup.sh
@@ -0,0 +1,34 @@
+#!/data/data/com.termux/files/usr/bin/bash -e
+
+## Setup Colors
+reset='\033[0m'
+red='\033[1;31m'
+blue='\033[1;34m'
+yellow='\033[1;33m'
+
+## Setup URL for download Linux FS 
+URL="https://dl.fedoraproject.org/pub/fedora/linux/releases/29/Container/$(uname -m)/images/"
+file="Fedora-Container-Minimal-Base-29-1.2.aarch64.tar.xz"
+FS="$HOME/.mytermux/linux/fedora/linux-fs"
+
+## Download compressed Linux FS
+printf "$yellow [*] Download: $file ...$reset\n"
+rm -rf $FS
+mkdir -p $FS  && cd $FS
+curl --progress-bar -L --fail --retry 4 -O "$URL/$file" -o $file
+
+## Extract Linux file-system
+printf "$yellow [*] Extract file-system$reset\n"
+proot --link2symlink tar -xJf ${file} --exclude='dev'||:
+
+## Remove compressed Linux FS
+chmod +rwx ../linux-fs root
+rm $file
+cd ..
+
+## Configure linux
+printf "$yellow [*] Configure linux$reset\n"
+echo 'nameserver 8.8.8.8' > $FS/etc/resolv.conf
+
+printf "\n"
+
---
+++ linux/fedora/start.sh
@@ -0,0 +1,47 @@
+#!/data/data/com.termux/files/usr/bin/bash -e
+unset LD_PRELOAD
+fs="$HOME/.mytermux/linux/fedora/linux-fs"
+sdcard=$(ls /storage | grep -E '[0-9]+')
+gitea="$HOME/.mytermux/gitea-app"
+java="$HOME/.mytermux/java-vm"
+
+## Setup Colors
+reset='\033[0m'
+red='\033[1;31m'
+blue='\033[1;34m'
+yellow='\033[1;33m'
+
+if [ ! -d $fs ]; then
+   printf "$yellow [*] You need to run setup-centos to create linux-fs\n$reset"
+   exit $?
+fi
+
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -b /dev"
+command+=" -b /sys"
+command+=" -b /proc"
+command+=" -b /storage"
+command+=" -b /storage/$sdcard/Android/data/com.termux:/root/sdcard"
+command+=" -b /storage/emulated/0:/root/internal"
+
+[[ -d $gitea ]] && \
+command+=" -b $gitea:/root/gitea-app"
+
+[[ -d $java ]] && \
+command+=" -b $java:/root/java-vm"
+
+command+=" -b $HOME"
+command+=" -r $fs"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" TERM=$TERM"
+command+=" LANG=C.UTF-8"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin"
+command+=" /bin/sh --login"
+echo $command
+printf "$yellow [*] $(cat $fs/etc/fedora-release)\n$reset"
+exec $command
+
