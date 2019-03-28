d97570ca1fba4ef97317b6c964083d92b21741a2
wharsojo <wharsojo@gmail.com>
2019-03-28 22:35:48 +0800

    cleanup mtx.sh

---
+++ scripts/mtx-info.sh
@@ -0,0 +1,22 @@
+if [ $# -lt 2 ]; then
+   if [ $# -eq 1 ]; then
+      printf "$red incomplete [arguments], at least one argument\n\n$reset"
+   fi
+   printf "$blue"
+   printf "mtx	v0.1 \n"
+   printf "Use mtx [options] <command> [arguments] \n\n"
+   printf "Commands:\n"
+   printf "i/install	alpine debian centos fedora kali ubuntu\n"
+   printf "u/uninstall	alpine debian centos fedora kali ubuntu\n"
+   printf "s/start    	alpine\n"
+   printf "d/download	gitea\n"
+   printf "r/remove	gitea\n\n"
+   printf "Example - i/install:\n"
+   printf "* mtx install alpine\n"
+   printf "* mtx -c i centos\n"
+   printf "\n"
+   printf "Example - d/download:\n"
+   printf "* mtx d gitea\n"
+   printf "\n"
+fi
+
--- scripts/mtx.sh
+++ scripts/mtx.sh
@@ -1,117 +1,64 @@
 #!/data/data/com.termux/files/usr/bin/bash -e
+. $HOME/.mytermux/linux/_modules/init-var-colors.sh
+. $HOME/.mytermux/scripts/mtx-info.sh $*
 
-## Setup Colors
-reset='\033[0m'
-red='\033[1;31m'
-blue='\033[1;34m'
-yellow='\033[1;33m'
-
-
-if [ $# -lt 2 ]; then
-   if [ $# -eq 1 ]; then
-      printf "$red incomplete [arguments], at least one argument\n\n$reset"
-   fi
-   printf "$blue"
-   printf "mtx	v0.1 \n"
-   printf "Use mtx [options] <command> [arguments] \n\n"
-   printf "Commands:\n"
-   printf "i/install	alpine debian centos fedora kali ubuntu\n"
-   printf "u/uninstall	alpine debian centos fedora kali ubuntu\n"
-   printf "s/start    	alpine\n"
-   printf "d/download	gitea\n"
-   printf "r/remove	gitea\n\n"
-   printf "Example - i/install:\n"
-   printf "* mtx install alpine\n"
-   printf "* mtx -c i centos\n"
-   printf "\n"
-   printf "Example - d/download:\n"
-   printf "* mtx d gitea\n"
-   printf "\n"
+function multiple_commands() {
+if [ ! "$COMMAND" == "" ]; then
+  printf "$red error multiple commands$reset\n\n"
+  exit 1
 fi
+}
 
 OPTIONS=""
 POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
-fs="$HOME/.mytermux"
 
-case $key in
+function install_linux() {
+  CMD="$fs/linux/$key/setup.sh $OPTIONS"
+  CMD2="~/.mytermux/linux/$key/setup.sh $OPTIONS"
+  printf "$yellow [*] $CMD2\n$reset"
+  $CMD
+  if [[ $OPTIONS == *"--configure"* ]]; then
+    echo configure...
+    $fs/linux/$key/configure.sh 
+  fi
+}
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  fs="$HOME/.mytermux"
+
+  case $key in
     -r|--remote)
-    OPTIONS+="--remote "
-    shift # pass options
-    ;;
+      OPTIONS+="--remote ";;
     -c|--configure)
-    OPTIONS+="--configure "
-    shift # pass options
-    ;;
-    i|install)
-    if [ ! "$COMMAND" == "" ]; then
-       printf "$red error multiple commands$reset\n\n"
-       exit 1
-    fi
-    COMMAND="install"
-    shift # pass value
-    ;;
+      OPTIONS+="--configure ";;
     s|start)
-    if [ ! "$COMMAND" == "" ]; then
-       printf "$red error multiple commands$reset\n\n"
-       exit 1
-    fi
-    COMMAND="start"
-    shift # pass value
-    ;;
+      multiple_commands && COMMAND="start";;
+    i|install)
+      multiple_commands && COMMAND="install";;
     u|uninstall)
-    if [ ! "$COMMAND" == "" ]; then
-       printf "$red error multiple commands$reset\n\n"
-       exit 1
-    fi
-    COMMAND="uninstall"
-    shift # pass value
-    ;;
-    r|remove)
-    if [ ! "$COMMAND" == "" ]; then
-       printf "$red error multiple commands$reset\n\n"
-       exit 1
-    fi
-    COMMAND="remove"
-    shift # pass value
-    ;;
+      multiple_commands && COMMAND="uninstall";;
     d|download)
-    if [ ! "$COMMAND" == "" ]; then
-       printf "$red error multiple commands$reset\n\n"
-       exit 1
-    fi
-    COMMAND="download"
-    shift # past value
-    ;;
+      multiple_commands && COMMAND="download";;
+    r|remove)
+      multiple_commands && COMMAND="remove";;
     *)
-    case $COMMAND in
-    	install)
-    	CMD="$fs/linux/$key/setup.sh $OPTIONS"
-    	CMD2="~/.mytermux/linux/$key/setup.sh $OPTIONS"
-    	printf "$yellow [*] $CMD2\n$reset"
-    	$CMD
-    	if [[ $OPTIONS == *"--configure"* ]]; then
-       	   echo configure...
-           $fs/linux/$key/configure.sh 
-        fi
-	;;
+      case $COMMAND in
         start)
-	$fs/linux/$key/start.sh
-	;;
-        remove)
-	printf "$red [*] remove $key"
-	rm -rf $fs/$key
-	;;
+	  $fs/linux/$key/start.sh;;
+    	install)
+          install_linux;;
         uninstall)
-	printf "$red [*] delete $key/linux-fs\n$reset"
-	rm -rf $fs/linux/$key/linux-fs
-	;;
-    esac
-    shift # past value
-    ;;
-esac
+	  printf "$red [*] delete $key/linux-fs\n$reset"
+	  rm -rf $fs/linux/$key/linux-fs;;
+        download)
+	  $fs/scripts/$key.sh;;
+        remove)
+	  printf "$red [*] remove $key"
+	  rm -rf $fs/$key;;
+      esac;;
+  esac
+  shift # past value
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 printf "\n"
