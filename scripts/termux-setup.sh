9a3b4e1cbd7aa8ee04d9e9e09a7ab6cf74e87337
wharsojo <wharsojo@gmail.com>
2019-03-29 00:40:13 +0800

    update  scripts

--- scripts/termux-setup.sh
+++ scripts/termux-setup.sh
@@ -1,10 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash -e
 
-## Setup Colors printf "\x1b[38;2;255;100;0mTRUECOLOR\x1b[0m\n"
-reset='\x1b[0m'
-red='\x1b[1;31m'
-blue='\x1b[1;34m'
-yellow='\x1b[1;33m'
+## Setup Colors 
+. $HOME/.mytermux/linux/_modules/init-var-colors.sh
 
 github="https://raw.githubusercontent.com"
 ohMyZsh="$github/robbyrussell/oh-my-zsh/master/tools/install.sh"
@@ -18,8 +15,8 @@ termux-setup-storage
 
 ## Setup default shell & startup 
 printf "$yellow [*] Setup default shell & startup$reset\n"
-ln -s ~/.mytermux/scripts/mtz.sh $PREFIX/bin/mtx
-cp zshenv ~/.zshenv
+ln -s $HOME/.mytermux/scripts/mtx.sh $PREFIX/bin/mtx
+cp zshenv $HOME/.zshenv
 chsh -s zsh
 
 ## Install oh-my-zsh on termux
